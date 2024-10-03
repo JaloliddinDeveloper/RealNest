9a8d05dc70bbfa37f6991d660c21b1802eac896c
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-03 22:13:26 +0500

    FRONT: Add Novigation bar

--- RealNest.Web/Controllers/HomeController.cs
+++ RealNest.Web/Controllers/HomeController.cs
@@ -3,14 +3,39 @@
 // Free To Use To Find Comfort And Peace
 //--------------------------------------------------
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealNest.Web.Models.Foundations.Users;
+using RealNest.Web.Models.ViewModels;
 
 namespace RealNest.Web.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public async ValueTask<IActionResult> Create(UserViewModel viewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = new User
+        //        {
+        //            Username = viewModel.Username,
+        //            Email = viewModel.Email
+        //        };
+        //       await this.
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(viewModel);
+        //}
     }
 }
