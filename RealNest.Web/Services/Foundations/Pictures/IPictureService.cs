043afb94fcb9650b9ee129faf5545c99d68e575e
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-03 20:40:40 +0500

    FOUNDATIONS: CRUD Picture

---
+++ RealNest.Web/Services/Foundations/Pictures/IPictureService.cs
@@ -0,0 +1,18 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+
+using RealNest.Web.Models.Foundations.Pictures;
+
+namespace RealNest.Web.Services.Foundations.Pictures
+{
+    public interface IPictureService
+    {
+        ValueTask<Picture> AddPictureAsync(Picture picture);
+        IQueryable<Picture> RetrieveAllPictures();
+        ValueTask<Picture> RetrievePictureByIdAsync(int pictureId);
+        ValueTask<Picture> ModifyPictureAsync(Picture picture);
+        ValueTask<Picture> RemovePictureAsync(int houseId);
+    }
+}
