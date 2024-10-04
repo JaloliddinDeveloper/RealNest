6af36561f63b25cd507090523a001c7127609dac
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-05 02:10:25 +0500

    CODE RUB:Fix Something

--- RealNest.Web/Models/ViewModels/UserViewModel.cs
+++ RealNest.Web/Models/ViewModels/UserViewModel.cs
@@ -2,12 +2,19 @@
 // Copyright (c) Coalition Of Good-Hearted Engineers
 // Free To Use To Find Comfort And Peace
 //--------------------------------------------------
+using System.ComponentModel.DataAnnotations;
+
 namespace RealNest.Web.Models.ViewModels
 {
     public class UserViewModel
     {
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
     }
 }
