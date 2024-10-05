b60f4f387116abc9f9ae308fbbf62aba9fc4341e
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-06 00:42:09 +0500

    Add Method SingIn And SingUp Imlement Basic

--- RealNest.Web/Controllers/AccountController.cs
+++ RealNest.Web/Controllers/AccountController.cs
@@ -3,40 +3,21 @@
 // Free To Use To Find Comfort And Peace
 //--------------------------------------------------
 using Microsoft.AspNetCore.Mvc;
-using RealNest.Web.Models.Foundations.Users;
-using RealNest.Web.Models.ViewModels;
-using RealNest.Web.Services.Foundations.Users;
 
 namespace RealNest.Web.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly IUserService userService;
-
-        public AccountController(IUserService userService) =>
-            this.userService = userService;
-
         [HttpGet]
         public IActionResult SignUp()
         {
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> SignUp(RegisterViewModel model)
+        [HttpGet]
+        public IActionResult SignIn()
         {
-            if (ModelState.IsValid)
-            {
-                var user = new User
-                {
-                    Username = model.Username,
-                    Email = model.Email,
-                    Password = model.Password,
-                };
-                var result = await userService.AddUserAsync(user);
-                return RedirectToAction("Index", "Home");
-            }
-            return View(model);
+            return View();
         }
     }
 }
