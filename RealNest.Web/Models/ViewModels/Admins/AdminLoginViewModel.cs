88cd6bed146073ed44df63d5979dcb079662f3fb
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-12-12 23:42:48 +0500

    CONTROLLER: LoginAdmin Implement

---
+++ RealNest.Web/Models/ViewModels/Admins/AdminLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealNest.Web.Models.ViewModels.Admins
+{
+    public class AdminLoginViewModel
+    {
+        [Required]
+        public string AdminName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
